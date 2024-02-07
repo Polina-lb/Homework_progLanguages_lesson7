d934740a597130145ebdcd15bad767fb031f5271
PL <177709@mail.ru>
2024-02-07 22:03:47 +0300

    The first task has been completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте значения M и N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+void OutputNumbers(int m, int n)
+{
+    if (m == n)
+    {
+        Console.Write($"{m}  ");
+        return;
+    }
+    else if (m < n)
+    {
+        Console.Write($"{m}  ");
+        OutputNumbers(m + 1, n);
+    }
+    else if (m > n)
+    {
+        Console.Write($"{m}  ");
+        OutputNumbers(m - 1, n);
+    }
+}
+Console.WriteLine("Введите натуральное число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+OutputNumbers(m, n);
