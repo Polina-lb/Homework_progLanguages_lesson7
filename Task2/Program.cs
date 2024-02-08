5e996a7744ade32ee7e9b34fabc12f77368818c7
PL <177709@mail.ru>
2024-02-09 01:16:36 +0300

    The second task has been completed

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int CalcAckermann(int n, int m)
+{
+    if (n==0)
+    {
+        return m+1;
+    }
+    else if (m==0)
+    {
+        return CalcAckermann(n-1, 1);
+    }
+    else{
+        int result = CalcAckermann(n-1, CalcAckermann(n, m-1));
+        return result;
+    }
+}
+int resultCalcAckermann = CalcAckermann(3,2);
+Console.WriteLine(resultCalcAckermann);
